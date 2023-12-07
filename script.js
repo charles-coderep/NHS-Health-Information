82b614557e56913fa16948fdbc479cb1c8e62f8c
charles-coderep <75278224+charles-coderep@users.noreply.github.com>
2023-12-07 00:09:13 +0000

    final changes for the day

--- script.js
+++ script.js
@@ -19,35 +19,41 @@ curl -X POST https://api.nhs.uk/service-search/search?api-version=1
 
 document.addEventListener("DOMContentLoaded", (event) => {
     const searchButton = document.getElementById('searchButton');
-    searchButton.addEventListener('click', function () {
-        const postcode = document.getElementById('postcode').value;
-    })
-    const url = new URL(`https://api.nhs.uk/service-search/search-postcode-or-place?api-version=1&search=${postcode}`);
-    //const postcode = url.searchParams.get('search');
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "subscription-key": "81e7f4069d6d4505b3415d1708b39cac"
-        },
-        body: JSON.stringify({
-            "select": "OrganisationName",
-            "top": 25,
-            "skip": 0,
-            "count": true
-        })
-    })
-        .then(response => {
-            if (!response.ok) {
-                return response.json()
-                    .then(errorData => {
-                        throw new Error(`${response.status}: ${errorData.errorName}. ${errorData.errorText}`);
-                    });
-            }
-            return response.json();
-        })
-        .then(data => console.log(data)).
-        catch(error => console.error(error));
-})
+    console.log(searchButton);
+    if (searchButton) {
+        searchButton.addEventListener('click', function () {
+            const postcode = document.getElementById('postcode').value;
 
+            const url = new URL(`https://api.nhs.uk/service-search/search-postcode-or-place?api-version=1&search=${postcode}`);
+            //const postcode = url.searchParams.get('search');
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "subscription-key": "81e7f4069d6d4505b3415d1708b39cac"
+                },
+                body: JSON.stringify({
+                    "select": "OrganisationName",
+                    "top": 25,
+                    "skip": 0,
+                    "count": true
+                })
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        return response.json()
+                            .then(errorData => {
+                                throw new Error(`${response.status}: ${errorData.errorName}. ${errorData.errorText}`);
+                            });
+                    }
+                    return response.json();
+                })
+                .then(data => console.log(data)).
+                catch(error => console.error(error));
+        })
+    }
+    else {
+        console.log('search button not found')
+    }
+})
 
